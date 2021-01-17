17aabed8cfba2c6d33eb839c5d40137ca9b30255
Jesús Gallego Irles <jesusgallego@outlook.com>
2021-01-17 15:51:26 +0100

    Initial Commit

---
+++ Shared/MTG_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTG_CounterApp.swift
+//  Shared
+//
+//  Created by Jesús Gallego Irles on 17/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MTG_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
